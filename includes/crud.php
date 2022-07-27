42a171c426e55ab1d964363af81e5ab3fe8b7e91
jayaprasad356 <jayaprasad356@gmail.com>
2022-07-27 22:13:29 +0530

    crud

--- includes/crud.php
+++ includes/crud.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('DOMAIN_URL', 'http://autobot.greymatterworks.in/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
+define('DOMAIN_URL', 'https://autobot.greymatterworks.in/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', '12345678'); /* chnage to your secret key here set it to anything you like*/
 
@@ -16,8 +16,8 @@ class Database
 
     private $db_host = "localhost";  // Change as required
     private $db_user = "u743445510_autobot";  // Change as required
-    private $db_pass = "u743445510_autobot";  // Change as required
-    private $db_name = "Autobot@2022";    // Change as required
+    private $db_pass = "Autobot@2022";  // Change as required
+    private $db_name = "u743445510_autobot";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
