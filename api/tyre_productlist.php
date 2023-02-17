56b7815ae39648560e20bd8ae6cc5f312b93323d
divaharsha <divakarvan03@gmail.com>
2023-02-17 13:57:50 +0530

    tyre productlist api updated

--- api/tyre_productlist.php
+++ api/tyre_productlist.php
@@ -12,88 +12,53 @@ include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
 
+$tyre_type = isset($_POST['tyre_type']) && !empty($_POST['tyre_type']) ? $db->escapeString($_POST['tyre_type']) : "";
+$wheel = isset($_POST['wheel']) && !empty($_POST['wheel']) ? $db->escapeString($_POST['wheel']) : "";
 
-if(isset($_POST['tyre_type']) && !empty($_POST['tyre_type'])){
-    $tyre_type = $db->escapeString($_POST['tyre_type']);
-    $wheel = $db->escapeString($_POST['wheel']);
+
+if (!empty($tyre_type) && !empty($wheel)) {
     $sql = "SELECT * FROM `tyre_products` WHERE tyre_type='$tyre_type' AND wheel='$wheel' AND status=1";
-    $db->sql($sql);
-    $res = $db->getResult();
-    $num = $db->numRows($res);
-    if ($num >= 1) {
-        foreach ($res as $row) {
-            $temp['id'] = $row['id'];
-            $temp['brand'] = $row['brand'];
-            $temp['size'] =$row['size'];
-            $temp['wheel_type'] = $row['wheel'];
-            $temp['pattern'] =$row['pattern'];
-            $temp['tyre_type'] = $row['tyre_type'];
-            $temp['amount'] =$row['amount'];
-            $temp['delivery_charges'] = $row['delivery_charges'];
-            $temp['fitting_charges'] = $row['fitting_charges'];
-            $temp['actual_price'] = $row['actual_price'];
-            $temp['final_price'] = $row['final_price'];
-            $temp['image'] = DOMAIN_URL . $row['image'];
-            $temp['status'] = $row['status'];
-            if($row['status']==1){
-                $temp['status'] ='Available';
-            }
-            else{
-                $temp['status'] ='Not-Available';
-            }
-            $rows[] = $temp;
-            
+} elseif (!empty($tyre_type) || !empty($wheel)) {
+    $sql = "SELECT * FROM `tyre_products` WHERE tyre_type='$tyre_type' OR wheel='$wheel' AND status=1";
+} else {
+    $sql = "SELECT * FROM `tyre_products` WHERE status=1";
+}
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if($num>=1){
+    foreach($res as $row){
+        $temp['id'] = $row['id'];
+        $temp['brand'] = $row['brand'];
+        $temp['size'] =$row['size'];
+        $temp['wheel_type'] = $row['wheel'];
+        $temp['pattern'] =$row['pattern'];
+        $temp['tyre_type'] = $row['tyre_type'];
+        $temp['mrp'] =$row['amount'];
+        $temp['delivery_charges'] = $row['delivery_charges'];
+        $temp['fitting_charges'] = $row['fitting_charges'];
+        // $temp['actual_price'] = $row['actual_price'];
+        $temp['price'] = $row['amount'];
+        $temp['image'] = DOMAIN_URL.$row['image'];
+        $temp['status'] = $row['status'];
+        if($row['status']==1){
+            $temp['status'] ='Available';
         }
-
-        $response['success'] = true;
-        $response['message'] = "Tyre Products listed Successfully";
-        $response['data'] = $rows;
-        print_r(json_encode($response));
-
-    }else{
-        $response['success'] = false;
-        $response['message'] = "No Tyre Products Found";
-        print_r(json_encode($response));
-
+        else{
+            $temp['status'] ='Not-Available';
+        }
+        $rows[] = $temp;
+        
     }
+    $response['success'] = true;
+    $response['message'] = "Tyre Products listed Successfully";
+    $response['data'] = $rows;
+    print_r(json_encode($response));
 }
 else{
-    $sql = "SELECT * FROM `tyre_products` WHERE status=1";
-    $db->sql($sql);
-    $res = $db->getResult();
-    $num = $db->numRows($res);
-    if ($num >= 1) {
-        foreach ($res as $row) {
-            $temp['id'] = $row['id'];
-            $temp['brand'] = $row['brand'];
-            $temp['size'] =$row['size'];
-            $temp['wheel_type'] = $row['wheel'];
-            $temp['pattern'] =$row['pattern'];
-            $temp['tyre_type'] = $row['tyre_type'];
-            $temp['amount'] =$row['amount'];
-            $temp['delivery_charges'] = $row['delivery_charges'];
-            $temp['fitting_charges'] = $row['fitting_charges'];
-            $temp['actual_price'] = $row['actual_price'];
-            $temp['final_price'] = $row['final_price'];
-            $temp['image'] = DOMAIN_URL . $row['image'];
-            $temp['status'] = $row['status'];
-            if($row['status']==1){
-                $temp['status'] ='Available';
-            }
-            else{
-                $temp['status'] ='Not-Available';
-            }
-            $rows[] = $temp;
-            
-        }
-
-        $response['success'] = true;
-        $response['message'] = "Tyre Products listed Successfully";
-        $response['data'] = $rows;
-        print_r(json_encode($response));
-
-    }
+    $response['success'] = false;
+    $response['message'] = "No Tyre Products Found";
+    print_r(json_encode($response));
 }
-
 
 ?>
